40dda55c1d90f82cb2b2078099ad0ebed5996e71
ssledz <slawomir.sledz@gmail.com>
2015-01-27 00:26:27 +0100

    bzz

--- ms-document-viewer-core/src/main/java/pl/softech/doc/Application.java
+++ ms-document-viewer-core/src/main/java/pl/softech/doc/Application.java
@@ -2,10 +2,12 @@ package pl.softech.doc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "classpath:applicationContext-integration.xml" })
 public class Application {
-    public static void main(String[] args) {
-	SpringApplication.run(Application.class, args);
-    }
+	public static void main(final String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
