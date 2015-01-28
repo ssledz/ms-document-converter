8e0e49f7bb88e776e1129e0dee34220d7c301f87
Slawomir Sledz <slawomir.sledz@contractors.roche.com>
2015-01-28 22:36:07 +0100

    bzz

--- ms-document-viewer-core/src/main/java/pl/softech/doc/PowerPointDocumentConverter.java
+++ ms-document-viewer-core/src/main/java/pl/softech/doc/PowerPointDocumentConverter.java
@@ -9,43 +9,65 @@ import java.io.OutputStream;
 
 import javax.imageio.ImageIO;
 
+import org.apache.poi.util.Units;
 import org.apache.poi.xslf.usermodel.XMLSlideShow;
 import org.apache.poi.xslf.usermodel.XSLFSlide;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.RectangleReadOnly;
 import com.lowagie.text.pdf.PdfWriter;
 
 public class PowerPointDocumentConverter implements DocumentConverter {
 
+    private Graphics2D createGraphics(BufferedImage imgBuffer) {
+	Graphics2D g2 = imgBuffer.createGraphics();
+	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+	g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+	g2.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+	g2.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+	g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+	return g2;
+    }
+
     @Override
     public void convertToPdf(AbstractDocument document, OutputStream out) throws Exception {
 
-	Document pdf = new Document();
-	PdfWriter writer = PdfWriter.getInstance(pdf, out);
+	XMLSlideShow ppt = new XMLSlideShow(document.getContentInputStream());
+	Dimension pageSize = ppt.getPageSize();
+
+	int margin = 36;
+
+	float aWidth = (float) (pageSize.getWidth() - 2 * margin);
+	float aHeight = (float) (pageSize.getHeight() - 2 * margin);
+	
+	int width = Units.toEMU(aWidth) / Units.EMU_PER_PIXEL;
+	int height = Units.toEMU(aHeight) / Units.EMU_PER_PIXEL;
+
+	Document pdf = new Document(new RectangleReadOnly((float) pageSize.getWidth(), (float) pageSize.getHeight()),
+		margin, margin, margin, margin);
+
+	PdfWriter.getInstance(pdf, out);
 
 	pdf.open();
 
-	XMLSlideShow ppt = new XMLSlideShow(document.getContentInputStream());
-	Dimension pageSize = ppt.getPageSize();
-	BufferedImage imgBuffer = new BufferedImage((int) pageSize.getWidth(), (int) pageSize.getHeight(),
-		BufferedImage.TYPE_INT_ARGB);
-
-	Graphics2D g2 = imgBuffer.createGraphics();
-	g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-			RenderingHints.VALUE_ANTIALIAS_ON);
-	g2.setRenderingHint(RenderingHints.KEY_RENDERING,
-			RenderingHints.VALUE_RENDER_QUALITY);
-	g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-			RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-	
 	for (XSLFSlide slide : ppt.getSlides()) {
+
+	    BufferedImage imgBuffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+
+	    Graphics2D g2 = createGraphics(imgBuffer);
 	    slide.draw(g2);
 	    pdf.add(new Paragraph());
 	    ByteArrayOutputStream otmp = new ByteArrayOutputStream();
 	    ImageIO.write(imgBuffer, "png", otmp);
-	    pdf.add(Image.getInstance(otmp.toByteArray()));
+	    Image img = Image.getInstance(otmp.toByteArray());
+	    img.setBorder(RectangleReadOnly.NO_BORDER);
+	    img.scaleToFit(aWidth, aHeight);
+	    img.setAlignment(Image.MIDDLE);
+	    pdf.add(img);
+
 	}
 
 	pdf.close();
